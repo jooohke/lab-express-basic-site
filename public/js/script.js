65a0314afe5b87b89b5cc19efb20f287689934dc
Johanna Angot <63612183+jooohke@users.noreply.github.com>
2021-07-20 11:31:09 +0200

    get random lyrics done

--- public/js/script.js
+++ public/js/script.js
@@ -1,21 +1,39 @@
-
-console.log("lyricsList : " + document.querySelector("p"))
-
 axios
   .get("https://taylorswiftapi.herokuapp.com/get-all")
 
   .then((response) => {
     // console.log('Response from API is: ' + response);
 
-    const lyrics = response.data[0]
 
-    console.log('a single lyrics: ', lyrics)
-    console.log(lyricsList)
+    // RANDOM LYRICS
 
-    // lyrics.forEach((lyric) => {
-      lyricsList.textContent = lyrics.song
-    // });
+    const randomLyrics = document.getElementById("random-lyrics")
+    const randomLyricsSong = document.querySelector("#random-lyrics-info span:first-child")
+    const randomLyricsAlbum = document.querySelector("#random-lyrics-info span:last-child")
+    const randomLyricsBtn = document.getElementById("random-lyrics-btn")
+
+    function getRandomLyrics() {
+      const getRandomIndex = Math.floor(Math.random() * response.data.length);
+      const randomLyricsContent = response.data[getRandomIndex]
+      // if (randomLyricsContent.quote.includes(" / ")) randomLyrics.textContent = randomLyricsContent.quote.replace(" / ", "\n");
+      randomLyrics.textContent = randomLyricsContent.quote;
+      randomLyricsSong.textContent = randomLyricsContent.song;
+      randomLyricsAlbum.textContent = randomLyricsContent.album;
+    }
+
+    randomLyricsBtn.onclick = getRandomLyrics
+
+    console.log("So I punched a hole in the roof / Let the flood carry away all my pictures of you.".replace(" / ", "\n"))
+
   })
   .catch(function (errorResponse) {
     console.log(errorResponse);
   });
+
+
+  /* WHAT TO DO 
+
+  * 
+
+
+  */
